cea754e6c3f06f2dea58cc4ce0492f33d3a796d8
sundayjohn78 <olorunferanmisunday@gmail.com>
2024-01-13 10:14:35 -0800

    few corrections on adding ;

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,33 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
-        Console.Write("What is your percentage score? (0-100)")
-        userInput = int.Parse(Console.ReadLine());
+        Console.Write("What is your percentage score? (0-100)");
+        int userInput = int.Parse(Console.ReadLine());
+
         if (userInput >= 90)
         {
-            Console.WriteLine("Grade A")
-            Console.WriteLine("Excellent Score!!!!")
+            Console.WriteLine("Grade A");
+            Console.WriteLine("Excellent Score!!!!");
         }
         else if (userInput >= 80)
         {
-            Console.WriteLine("Grade B")
-            Console.WriteLine("Excellent Score!!!!")
+            Console.WriteLine("Grade B");
+            Console.WriteLine("Excellent Score!!!!");
         }
         else if (userInput >= 70)
         {
-            Console.WriteLine("Grade C")
-            Console.WriteLine("Excellent Score!!!!")
+            Console.WriteLine("Grade C");
+            Console.WriteLine("Excellent Score!!!!");
         }
         else if (userInput >= 60)
         {
-            Console.WriteLine("Grade D")
-            Console.WriteLine("You can try better, anticipate for 70% score")
+            Console.WriteLine("Grade D");
+            Console.WriteLine("You can try better, anticipate for a 70% score");
         }
-        else (userInput < 60)
+        else
         {
-            Console.WriteLine("Grade F")
-            Console.WriteLine("Very low score, you can try better, anticipate for 70% score")
+            Console.WriteLine("Grade F");
+            Console.WriteLine("Very low score, you can try better, anticipate for a 70% score");
         }
-        
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,23 +7,23 @@ class Program
         Console.WriteLine("Hello Prep3 World!");
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(1, 11);
-        int guess = -1
+        int guess = -1;
+
         while (guess != number)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
+
             if (guess > number)
             {
-                Console.WriteLine("Guess a Lower Number")
+                Console.WriteLine("Guess a Lower Number");
             }
             else if (guess < number)
             {
-                Console.WriteLine("Guess a Lower Number")
+                Console.WriteLine("Guess a Higher Number");
             }
         }
-            else
-            {
-                console.WriteLine("You've guessed the right number")
-            }
+
+        Console.WriteLine("You've guessed the right number");
     }
 }
