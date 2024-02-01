00e96423e09a6ada75d5ba3f318d618520327759
sundayjohn78 <olorunferanmisunday@gmail.com>
2024-02-01 10:52:54 -0800

    week04 learning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Samuel Sunday", "Division");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Lucie Sunday", "Fractions", "7.2", "7-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Hannah Sunday", "Yoruba History", "The Story of Moremi");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssinment.cs
@@ -0,0 +1,18 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        // Here we set any variables specific to the WritingAssignment class
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+}
